8f8ad5afdd731d53b19d51ef1412fc7176b6bf72
sahyan <tahirihasinarakotomanga@gmail.com>
2024-05-22 14:10:33 +0300

    Vehicule terminer

---
+++ Vehicule.cpp
@@ -0,0 +1,81 @@
+#include "Vehicule.hpp"
+#include <iostream>
+
+using namespace std;
+
+/*************************************************************/
+
+Vehicule::Vehicule()
+{
+
+}
+
+/*************************************************************/
+
+Vehicule::Vehicule(string marque, string couleur, int nombreDePLace)
+{
+    this->marque = marque;
+    this->couleur = couleur;
+    this->nombreDePLace = nombreDePLace;
+}
+
+/*************************************************************/
+
+Vehicule::~Vehicule()
+{
+
+}
+
+/*************************************************************/
+
+Vehicule::~Vehicule(string message)
+{
+    cout << "Destruction..." << endl;
+}
+
+/*************************************************************/
+
+void Vehicule::setMarque(string marque)
+{
+    this->marque = marque;
+    cout << "La marque a ete mis a jour..." << endl;
+}
+
+/*************************************************************/
+
+void Vehicule::setCouleur(string couleur)
+{
+    this->couleur = couleur;
+    cout << "La couleur a ete mis a jour..." << endl;
+}
+
+/*************************************************************/
+
+void Vehicule::setNombreDePlace(int nombreDePLace)
+{
+    this->nombreDePLace = nombreDePLace;
+    cout << "Le Nombre de place a ete mis a jour..." << endl;
+}
+
+/*************************************************************/
+
+string Vehicule::getMarque()
+{
+    return marque;
+}
+
+/*************************************************************/
+
+string Vehicule::getCouleur()
+{
+    return couleur;
+}
+
+/*************************************************************/
+
+int Vehicule::getNombreDePLace()
+{
+    return nombreDePLace;
+}
+
+/*************************************************************/
---
+++ Vehicule.hpp
@@ -0,0 +1,31 @@
+#ifndef VEHICULE_HPP
+#define VEHICULE_HPP
+
+class Vehicule
+{
+
+    private:
+        string marque;
+        string couleur;
+        int nombreDePLace;
+
+    public:
+        Vehicule();
+        Vehicule(string marque, string couleur, int nombreDePLace);
+        ~Vehicule(string message);
+        ~Vehicule();
+        
+        virtual void conduire() = 0;
+        virtual void affichageInfo() = 0;
+        
+        void setMarque(string marque);
+        void setCouleur(string couleur);
+        void setNombreDePlace(int nombreDePLace);
+
+        string getMarque();
+        string getCouleur();
+        int getNombreDePLace();
+
+};
+
+#endif
